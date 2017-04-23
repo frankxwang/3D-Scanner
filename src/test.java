c553e3b795d21d13ce3f67d7ada27561f4b9d3b1
thegithubgeek <wfrankw9@gmail.com>
2017-04-22 22:19:38 -0700

    Sutff

--- src/test.java
+++ src/test.java
@@ -1,9 +1,21 @@
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import com.googlecode.javacv.FFmpegFrameGrabber;
 
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		FFmpegFrameGrabber g = new FFmpegFrameGrabber("");
+		g.start();
 
+		for (int i = 0 ; i < 10 ; i++) {
+		    ImageIO.write(g.grab().getBufferedImage(), "png", new File("frame-dump/video-frame-" + i + ".png"));
+		}
+
+		g.stop();
 	}
 
 }
